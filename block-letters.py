16a0fbe81032770e26bd18270358a5836431fc8a
Kristal Kung <kristalkung@gmail.com>
2021-12-10 00:04:11 +0000

    added block letters

---
+++ block-letters.py
@@ -0,0 +1,25 @@
+# prints out my initials in ASCII art
+
+print('K   K K   K ')
+print('K  K  K  K  ')
+print('K K   K K')
+print('K     K')
+print('K K   K K')
+print('K  K  K  K')
+print('K   K K   K ')
+
+# Kristal Kung
+# fun fact: I am left handed
+
+
+name = """
+K   K  K   K
+K  K   K  K
+K K    K K
+K      K
+K K    K K
+K  K   K  K
+K   K  K   K
+"""
+
+print(name)
